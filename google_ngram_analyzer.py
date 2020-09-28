044e1cab9fdbe5047c505496e7cfd947b8641a02
agenc <attilagenc@gmail.com>
2020-09-27 17:28:56 -0700

    changed min/max constants, added keyword

--- google_ngram_analyzer.py
+++ google_ngram_analyzer.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 VALID_LANGUAGES = ['eng']
 MIN_YEAR = 1500
-MAX_YEAR = 2012
+MAX_YEAR = 2020
 goggle_ngram_viewer_dataset_folder = '/home/agenc/data/term/downloads/google_ngrams/1/'
 
 
@@ -22,6 +22,7 @@ def load_totals_data():
     data = []
     for i in list(range(MIN_YEAR, MAX_YEAR)):
         data.append(0)
+    max_year = MIN_YEAR
     with open(get_ngram_dataset_totals_file()) as infile:
         for line in infile:
             split_line = line.split("\t")
@@ -31,6 +32,9 @@ def load_totals_data():
                     year = int(triplet_values[0])
                     match_count = int(triplet_values[1])
                     data[year - MIN_YEAR] = data[year - MIN_YEAR] + match_count
+                    if year > max_year:
+                        max_year = year
+    print("Max year in totals:" + str(max_year))
     return data
 
 
@@ -40,6 +44,7 @@ def filter_data(keyword, start, end, split_line):
 
 
 def load_data(keyword, start, end):
+    max_year = MIN_YEAR
     data = [0] * (end - start)
     keyword = keyword.lower()
     with open(get_ngram_dataset_file(keyword)) as infile:
@@ -49,6 +54,9 @@ def load_data(keyword, start, end):
                 year = int(split_line[1])
                 # print(split_line[0], split_line[1], split_line[2], len(data), year-start)
                 data[year - start] = data[year - start] + int(split_line[2])
+                if year > max_year:
+                    max_year = year
+    print("Max year in data for keyword="+keyword+":" + str(max_year))
     return data
 
 
@@ -58,7 +66,9 @@ def compute_frequencies(totals, data, start):
         year = i + start
         total_count = totals[year - MIN_YEAR]
         if total_count > 0:
-            frequency = data[i] / total_count
+            frequency = float(data[i]) / float(total_count)
+            if frequency > 1:
+                print("Frequency=" + str(frequency) + " count=" + str(data[i]) + " total count=" + str(total_count))
             frequencies.append(frequency)
         else:
             frequencies.append(0)
@@ -80,9 +90,9 @@ def plot_graph(keyword, start, end, frequencies):
 
 def ngram_viewer(keyword, start, end, totals):
     data = load_data(keyword, start, end)
-    print(data)
+    # print(data)
     frequencies = compute_frequencies(totals, data, start)
-    print(frequencies)
+    # print(frequencies)
     plot_graph(keyword, start, end, frequencies)
 
 
