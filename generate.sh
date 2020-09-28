fb8e98920f7e90b5ab8148de39d06ac8705382d8
agenc <attilagenc@gmail.com>
2020-09-27 17:02:54 -0700

    added data generation script

---
+++ generate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+/home/agenc/venv/bin/python /home/agenc/git/data.mining.term/google_ngram_analyzer.py --startyear 1500 --endyear 2012 --lang eng --keyword Politics
+/home/agenc/venv/bin/python /home/agenc/git/data.mining.term/google_ngram_analyzer.py --startyear 1500 --endyear 2012 --lang eng --keyword Science
+/home/agenc/venv/bin/python /home/agenc/git/data.mining.term/google_ngram_analyzer.py --startyear 1500 --endyear 2012 --lang eng --keyword Mass Media
+/home/agenc/venv/bin/python /home/agenc/git/data.mining.term/google_ngram_analyzer.py --startyear 1500 --endyear 2012 --lang eng --keyword Religion
+/home/agenc/venv/bin/python /home/agenc/git/data.mining.term/google_ngram_analyzer.py --startyear 1500 --endyear 2012 --lang eng --keyword Economy
+/home/agenc/venv/bin/python /home/agenc/git/data.mining.term/google_ngram_analyzer.py --startyear 1500 --endyear 2012 --lang eng --keyword Law
+/home/agenc/venv/bin/python /home/agenc/git/data.mining.term/google_ngram_analyzer.py --startyear 1500 --endyear 2012 --lang eng --keyword Education
+/home/agenc/venv/bin/python /home/agenc/git/data.mining.term/google_ngram_analyzer.py --startyear 1500 --endyear 2012 --lang eng --keyword Health
+/home/agenc/venv/bin/python /home/agenc/git/data.mining.term/google_ngram_analyzer.py --startyear 1500 --endyear 2012 --lang eng --keyword Art
+/home/agenc/venv/bin/python /home/agenc/git/data.mining.term/google_ngram_analyzer.py --startyear 1500 --endyear 2012 --lang eng --keyword Sports
